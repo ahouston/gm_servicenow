2b82f2abe1f60f3e8cc8073983c8f2dfec14da23
ahouston@gmail.com <root@pluto.ngn.durban.gov.za>
2013-06-25 11:40:59 +0200

    Added the local JS require files

---
+++ GreaseMonkey/splitbutton.js
@@ -0,0 +1,32 @@
+$(function() {
+     
+
+    $( "#rerun" )
+      .button()
+      .click(function() {
+        alert( "Running the last action" );
+      })
+      .next()
+        .button({
+          text: false,
+          icons: {
+            primary: "ui-icon-triangle-1-s"
+          }
+        })
+        .click(function() {
+          var menu = $( this ).parent().next().show().position({
+            my: "left top",
+            at: "left bottom",
+            of: this
+          });
+          $( document ).one( "click", function() {
+            menu.hide();
+          });
+          return false;
+        })
+        .parent()
+          .buttonset()
+          .next()
+            .hide()
+            .menu();
+});

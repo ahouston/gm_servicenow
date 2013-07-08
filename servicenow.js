55eee79173e90756684d6dd2fe5d8eb566a18441
Allan Houston <ahouston@gmail.com>
2013-07-08 14:35:33 +0200

    Made includes more specific to stop race conditions...

--- servicenow.js
+++ servicenow.js
@@ -7,11 +7,11 @@
 // @require    file://C:/GreaseMonkey/jquery-ui.js
 // @resource   customCSS file://C:/GreaseMonkey/jquery-ui-1.10.3.custom.css
 // @description ServiceNow Actions
-// @include    *didataservices.service-now.com*
-// @include    https://didataservices.service-now.com/nav.do*
 // @include    https://didataservices.service-now.com/incident.do*
 // @include    https://didataservices.service-now.com/u_request.do*
 // @include    https://didataservices.service-now.com/change_request.do*
+// @include    https://didataservices.service-now.com/u_workload.do*
+// @include    https://didataservices.service-now.com/u_reminder.do*
 // @updateURL  https://raw.github.com/ahouston/gm_servicenow/master/metadata
 // @downloadURL https://raw.github.com/ahouston/gm_servicenow/master/servicenow.js
 // @copyright  2013, Allan Houston
